d0a8282520dddd16a81e3fd156d1925114faf958
SM43 <smukhade@redhat.com>
2021-02-16 10:52:57 +0530

    Add deployment files

--- cmd/user/main.go
+++ cmd/user/main.go
@@ -113,7 +113,7 @@ func main() {
 	switch *hostF {
 	case "localhost":
 		{
-			addr := "http://localhost:8080"
+			addr := "http://:8080"
 			u, err := url.Parse(addr)
 			if err != nil {
 				fmt.Fprintf(os.Stderr, "invalid URL %#v: %s\n", addr, err)
